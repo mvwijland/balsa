46b0646fde41a95a913ac887d7483faf4ca5bef5
Rüzgar Burak Topal <hello@ruzgar.me>
2019-10-01 12:28:17 +0300

    update additions

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@
 curl -s https://api.github.com/repos/balsa-team/balsa/releases/latest | grep "balsa.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 tar -xvf balsa.tar.gz
 rm balsa.tar.gz
+
+yarn && yarn build
+sudo systemctl restart balsa
